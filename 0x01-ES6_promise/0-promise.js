a5a7e7c58e081c0fc5ff2ec401895b21418d9d56
NezhaviAbderrahmane <cszamasu@gmail.com>
2024-04-10 18:49:47 +0000

    New

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,5 @@
 /*eslint-disable*/
 
 export default function getResponseFromAPI() {
-  return new Promise((res, rej) => {
-    if (5 > 2) {
-        res("G")
-    }
-    else
-        rej(Error("EO"))
-  });
+  return Promise.resolve("s")
 }
