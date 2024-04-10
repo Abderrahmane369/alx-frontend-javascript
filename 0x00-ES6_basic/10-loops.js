9a04f1a0448504a55d893b3d39cd9212110d1b59
NezhaviAbderrahmane <cszamasu@gmail.com>
2024-04-10 02:04:04 +0000

    New

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,7 @@
-/* eslint-disable */
-
 export default function appendToEachArrayValue(array, appendString) {
-  for (const _ of array.entries()) {
-    const value = _[1];
-    array[_[0]] = appendString + value;
+  for (const _ of array) {
+    const value = _;
+    array[array.indexOf(value)] = appendString + array[array.indexOf(value)];
   }
 
   return array;
