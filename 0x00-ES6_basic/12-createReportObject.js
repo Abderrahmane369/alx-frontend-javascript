fb64753e325e7043cd3cca32373a3721ee144386
NezhaviAbderrahmane <cszamasu@gmail.com>
2024-04-10 02:23:39 +0000

    New

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,8 +2,8 @@
 export default function createReportObject(employeesList) {
 	return {
 		allEmployees: employeesList,
-		getNumberOfDepartements(employeesList) {
-			return employeesList.length;
+		getNumberOfDepartments(employeesList) {
+			return Object.keys(employeesList).length;
 		}		
 	};
 }
