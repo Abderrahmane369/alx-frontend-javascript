249f6a5076e55987ac7fada521111083ec343704
NezhaviAbderrahmane <cszamasu@gmail.com>
2024-04-10 22:03:46 +0000

    New

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,1 +1,12 @@
-function handleResponseFromAPI(promise)
+export default function handleResponseFromAPI(promise) {
+    return promise.then(() => {
+        console.log('Got a response from the API');
+        return {
+
+            status: 200, body: 'success'
+        }
+    })
+    .catchr(() => {
+        return new Error();
+    })
+}
