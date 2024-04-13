5dba50e28a5d730229c33746380626c5cfb1dd15
NezhaviAbderrahmane <cszamasu@gmail.com>
2024-04-13 13:38:58 +0000

    A

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,7 +1,7 @@
 /*eslint-disable*/
-import Building from './5-building';
+import Building from './5-building.js';
 
-class SkyHighBuilding extends Building {
+export default class SkyHighBuilding extends Building {
   constructor (sqft, floors) {
     super(sqft);
     this._floors = floors;
