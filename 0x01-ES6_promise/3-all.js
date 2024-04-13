ca2b204d61c63ba16e6e1eeb9c1cbc4ab453f272
NezhaviAbderrahmane <cszamasu@gmail.com>
2024-04-13 22:02:55 +0000

    a

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,9 @@
 /*eslint-disable*/
-import { createUser, uploadPhoto } from './utils'
+import { createUser, uploadPhoto } from './utils.js'
 
 export default function handleProfileSignup () {
   Promise.all([uploadPhoto(), createUser()]).then((_) => {
     console.log(`${_[0].body} ${_[1].firstName} ${_[1].lastName}`)
   })
-    .catch(console.log('Signup system offline'))
+    .catch(() => console.log('Signup system offline'))
 }
