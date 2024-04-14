a2ff6ea5af81d02574894b92cda6d329643a21e2
NezhaviAbderrahmane <cszamasu@gmail.com>
2024-04-14 16:33:22 +0100

    a

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,15 @@
 /*eslint-disable*/
-export default function handleResponseFromAPI (promise) {
-  return promise.then(() => {
-    console.log('Got a response from the API');
-    return {
-      status: 200,
-      body: 'success'
-    };
-  }).catch(() => Error());
+import uploadPhoto from "./5-photo-reject";
+import signUpUser from "./4-user-promise";
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    const s = signUpUser(firstName, lastName);
+    const p = uploadPhoto(fileName);
+
+    return Promise.allSettled([s, p]).then(results => {
+        return results.map(result => ({
+            status: result.status,
+            value: result.status === 'fulfilled' ? result.value : result.reason
+        }));
+    });
 }
