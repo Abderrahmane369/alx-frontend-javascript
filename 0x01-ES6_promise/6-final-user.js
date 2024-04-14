a2f24863ac986f2668a6cac32e2b751c9a6533f5
NezhaviAbderrahmane <cszamasu@gmail.com>
2024-04-14 14:57:48 +0100

    a

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise';
+
+export default function handleProfileSignup (firstName, lastName, fileName) {
+  const s = signUpUser(firstName, lastName);
+  const p = uploadPhoto(fileName);
+
+  return Promise.allSettled([s, p]).then(r => r.map(_r => ({
+    status: _r.status,
+    value: _r.s === 'fulfilled' ? _r.value : _r.reason
+  })));
+};
