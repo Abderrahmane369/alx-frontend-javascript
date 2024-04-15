67cffc7812878f44899a4081e78eb3bc2a6cdbde
NezhaviAbderrahmane <cszamasu@gmail.com>
2024-04-15 22:36:25 +0100

    a

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 /*eslint-disable*/
 export default function cleanSet(set, startString) {
-    if (typeof startString !== 'string' || startString === '') {
+    if (startString === '') {
       return '';
     }
   
