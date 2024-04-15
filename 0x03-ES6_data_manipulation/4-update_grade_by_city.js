8b2e937d66169906a1bf9b0de11654fc3877e944
NezhaviAbderrahmane <cszamasu@gmail.com>
2024-04-15 19:15:17 +0100

    a

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,8 @@
 /*eslint-disable*/
 export default function updateStudentGradeByCity (list, city, grade) {
   return list.filter(_ => _.location == city ).map((v) => {
+    v.grade = "N/A"
     for (let g of grade) {
-        v.grade = "N/A"
       if (g.studentId == v.id) {
         v.grade = g.grade
       }
