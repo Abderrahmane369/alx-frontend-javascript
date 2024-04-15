72a3164280fb0f1c67e1d69dedb27751741ae3ed
NezhaviAbderrahmane <cszamasu@gmail.com>
2024-04-15 23:01:10 +0100

    a

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,6 @@
 /*eslint-disable*/
 export default function updateUniqueItems(map) {
-    if (!map instanceof Map) throw Error("Cannot process");
+    if (!(map instanceof Map)) throw Error("Cannot process");
     for (const [k, v] of map.entries()) {
         if (v === 1) map.set(k, 100);
     }
