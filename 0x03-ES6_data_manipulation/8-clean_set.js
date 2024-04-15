3f18833de3f8071cae1795202fa397f7c0174662
NezhaviAbderrahmane <cszamasu@gmail.com>
2024-04-15 22:33:39 +0100

    a

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,7 @@ export default function cleanSet(set, start) {
     if (!start) return "";
 
     return [...set]
-        .filter(_ => _.includes(start))
-        .map(_ => _.replace(start, ""))
+        .filter(_ => _.substr(0, start.length) == start)
+        .map(_ => _.substr(start.length))
         .join("-");
 }
