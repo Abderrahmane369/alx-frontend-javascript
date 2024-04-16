1dd0a2381d7aaef993b401c97f4b09951ca60dd7
NezhaviAbderrahmane <cszamasu@gmail.com>
2024-04-16 13:16:36 +0100

    a

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,15 @@
 /*eslint-disable*/
 export default function cleanSet(set, startString) {
-    if (typeof startString !== 'string' || startString === '') {
-      return '';
+    if (typeof startString !== "string" || startString === "") {
+        return "";
     }
-  
+
     const result = [...set]
-      .filter(item => item.startsWith(startString))
-      .map(item => item.slice(startString.length))
-      .join('-');
-  
+        .filter(
+            item => typeof item === "string" && item.startsWith(startString)
+        )
+        .map(item => item.slice(startString.length))
+        .join("-");
+
     return result;
-  }
+}
