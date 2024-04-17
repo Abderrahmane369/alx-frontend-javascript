80e1ea4eb058f336344b01ad48e13cdc74c45f38
NezhaviAbderrahmane <cszamasu@gmail.com>
2024-04-17 17:52:13 +0100

    a

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,10 +1,36 @@
 interface Student {
-	firstName: string;
-	lastName: string;
-	age: number;
-	location: string;
-	}
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
 
- const student1: Student, student2: Student;
- const arrayStudents = [student1, student2];
+const student1: Student = {
+        firstName: "A",
+        lastName: "A",
+        age: 1,
+        location: "A"
+    },
+    student2: Student = {
+        firstName: "A",
+        lastName: "A",
+        age: 1,
+        location: "A"
+    };
 
+const arrayStudents = [student1, student2];
+
+const table = document.createElement("table");
+
+for (const s of arrayStudents) {
+	const row = document.createElement("tr");
+	const td = document.createElement("td");
+	const td1 = document.createElement("td");
+
+	td.innerHTML = s.firstName;
+	td1.innerHTML = s.lastName;
+
+	row.appendChild(td);
+	row.appendChild(td1);
+	table.appendChild(row);
+}
