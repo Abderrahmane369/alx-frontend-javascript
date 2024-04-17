b8feb7c0ccf3b6156fd6b490c9ded85141a756ed
Abderrhamane <cszamasu@gmail.com>
2024-04-17 15:37:17 +0000

    a

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,10 @@
+interface Student {
+	firstName: string;
+	lastName: string;
+	age: number;
+	location: string;
+	}
+
+ const student1: Student, student2: Student;
+ const arrayStudents = [student1, student2];
+
